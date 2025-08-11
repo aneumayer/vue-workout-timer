93a5acb635fd142e70570624041f3a4ec0502cf9
Tony Neumayer <tony.neumayer@whro.org>
2025-08-11 11:00:46 -0400

    ➕ Add bootstrap and session timer component.

---
+++ src/components/SessionTimer.vue
@@ -0,0 +1,73 @@
+<script setup>
+import { ref, onUnmounted } from 'vue';
+
+let running = ref(false);
+let elapsed = ref(0);
+let sessionTime = ref('00:00');
+let intervalId = null;
+
+const formatTime = (secs) => {
+    const m = String(Math.floor(secs / 60)).padStart(2, '0');
+    const s = String(secs % 60).padStart(2, '0');
+    return `${m}:${s}`;
+};
+
+const updateTime = () => {
+    sessionTime.value = formatTime(elapsed.value);
+};
+
+const startTimer = () => {
+    if (!intervalId) {
+        intervalId = setInterval(() => {
+            elapsed.value++;
+            updateTime();
+        }, 1000);
+    }
+};
+
+const stopTimer = () => {
+    if (intervalId) {
+        clearInterval(intervalId);
+        intervalId = null;
+    }
+};
+
+const resetTimer = () => {
+    elapsed.value = 0;
+    stopTimer();
+    running.value = false;
+    sessionTime.value = '00:00';
+};
+
+const toggleTimer = () => {
+    running.value = !running.value;
+    if (running.value) {
+        startTimer();
+    } else {
+        stopTimer();
+    }
+};
+
+onUnmounted(() => {
+    stopTimer();
+});
+</script>
+
+<template>
+    <div class="container">
+        <div class="display">{{ sessionTime }}</div>
+        <div class="controls">
+            <button class="control-button" @click="toggleTimer">{{ running ? 'Pause' : 'Start' }}</button>
+            <button class="control-button" @click="resetTimer">Reset</button>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .display {
+        font-size: 3em;
+    }
+    .control-button {
+        margin: 0.5em;
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
+import * as bootstrap from 'bootstrap/dist/js/bootstrap.bundle';
+import 'bootstrap/dist/css/bootstrap.css';
+
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.provide('bootstrap', bootstrap);
+app.mount('#app');
